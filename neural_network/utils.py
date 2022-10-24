da21debae66405277a714aba041c375a2a0b6f92
Charbel Trad <charbeltrad622@gmail.com>
2022-10-24 19:57:36 +0200

    indicating the generated requests in the generated_requests.csv

--- neural_network/utils.py
+++ neural_network/utils.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from data import apply, response_test_1, response_test_2
+from data import apply, language, response_test_1, response_test_2
 import requests
 import urllib.parse
 import urllib.error
-
+import random
 
 class Connector:
     def __init__(self):
@@ -60,4 +60,47 @@ def fake_request(stored_requests, request):
 
 
 if __name__ == '__main__':
-    fake_request("../data/stored_requests.csv", [response_test_1, response_test_2])
+    city_distribution = pd.read_csv('../data/city_distribution.csv')
+
+    cities = city_distribution['city'].tolist()
+    cities_weights =city_distribution['nb_hotel'].tolist()
+
+    random_cities=random.choices(cities, weights=cities_weights, k=1)
+    random_city=random_cities[0]
+
+    random_date=random.randint(0,44)
+
+    random_language= random.choice(list(language.items()))[0]
+
+    random_mobile=random.randint(0,1)
+
+    possible_api_requests = pd.read_csv('../data/possible_api_requests.csv')
+    possible_api_requests = possible_api_requests.to_numpy().tolist()
+
+    new_request_raw =[random_city,random_language,random_date,random_mobile]
+    params_condition= new_request_raw in possible_api_requests
+
+    if params_condition:
+        raw_index=possible_api_requests.index(new_request_raw)
+        possible_api_requests.pop(raw_index)
+
+        api_requests_df = pd.DataFrame(possible_api_requests, columns=['city', 'language','date','mobile'])
+        api_requests_df.to_csv(f'../data/possible_api_requests.csv', index=False)
+
+        generated_requests = pd.read_csv('../data/generated_requests.csv')
+        generated_requests = generated_requests.to_numpy().tolist()
+        generated_requests.append(new_request_raw)
+        generated_requests_df = pd.DataFrame(generated_requests, columns=['city', 'language','date','mobile'])
+        generated_requests_df.to_csv(f'../data/generated_requests.csv', index=False)
+
+        params = {
+        "avatar_name": str(random_date),
+        "language": random_language,
+        "city": random_city,
+        "date": str(random_date),
+        "mobile": str(random_mobile),
+        }
+
+        print(params)
+    else:
+        print("Try again")
