b80cbffe180c714da534da73b8883cf24be44af9
Pertingens <augustinborne29@gmail.com>
2022-10-26 14:32:14 +0200

    test nn

--- neural_network/network.py
+++ neural_network/network.py
@@ -1,27 +1,38 @@
 import torch
 from torch import Tensor
 from torch.nn import Linear, ModuleList, MSELoss, Module
-from torch.nn.functional import relu,sigmoid,elu,silu
+from torch.nn.functional import relu, sigmoid, elu, silu
 from torch.optim import SGD
 import warnings
 from data import apply
 from dataset import prepare_dataloader
+from sklearn.metrics import mean_squared_error
 
 warnings.filterwarnings("ignore")
 
 
 class Model(Module):
-    def __init__(self, layer):
+    def __init__(self, layer=[10, 5, 1]):
         super().__init__()
-        self.layers = ModuleList()
-        for i in range(1, len(layer)):
-            self.layers.append(Linear(layer[i - 1], layer[i]))
+        # self.layers = ModuleList()
+        # for i in range(1, len(layer)):
+        #    self.layers.append(Linear(layer[i - 1], layer[i]))
+
+        self.layer_1 = Linear(10, 10)
+        self.layer_2 = Linear(10, 7)
+        self.layer_3 = Linear(7, 5)
+        self.layer_4 = Linear(5, 5)
+        self.layer_out = Linear(5, 1)
 
     def forward(self, x):
-        #print(self.layers)
-        for i in self.layers:
-            x = silu(i(x))
-            #print(x)
+        # for i in range(len(self.layers) - 1):
+        #    x = relu(self.layers[i](x))
+        # x = self.layers[-1](x)
+        x = silu(self.layer_1(x))
+        x = sigmoid(self.layer_2(x))
+        x = sigmoid(self.layer_3(x))
+        x = relu(self.layer_4(x))
+        x = self.layer_out(x)
         return x
 
 
@@ -47,19 +58,14 @@ def train(model, train_data, validation_data, learning_rate=0.1, loss_function=M
         for X_train, y_train in train_data:
             # zero the parameter gradients
             optimizer.zero_grad()
-
             # predict output from the model
             prediction = model(X_train)
-
             # calculate loss for the predicted output
             loss = loss_function(prediction, y_train)
-
             # backpropagate the loss
             loss.backward()
-
             # adjust parameters based on the calculated gradients
             optimizer.step()
-
             # update the loss value
             epoch_train_loss += loss.item()
 
@@ -68,6 +74,8 @@ def train(model, train_data, validation_data, learning_rate=0.1, loss_function=M
 
         loss_values.append(train_loss_value)
 
+        y_predicted = []
+        y_actual = []
         # Validation Loop
         with torch.no_grad():
             model.eval()
@@ -76,36 +84,42 @@ def train(model, train_data, validation_data, learning_rate=0.1, loss_function=M
                 val_loss = loss_function(prediction, y_val)
                 epoch_vall_loss += val_loss.item()
                 total += y_val.size(0)
-                epoch_accuracy += (prediction == y_val).sum().item()
+
+                y_predicted.extend(prediction.tolist())
+                y_actual.extend(y_val.tolist())
 
         # Calculate validation loss value
         val_loss_value = epoch_vall_loss / len(validation_data)
 
         # Calculate the accuracy
-        accuracy = (100 * epoch_accuracy / total)
+        # print(len(y_actual))
+        # print(len(y_predicted))
+        accuracy = mean_squared_error(y_actual, y_predicted, squared=False)
         accuracy_values.append(accuracy)
 
         # Saving the model
         if accuracy > max(accuracy_values):
             saveModel()
         print(
-            f"Epoch {epoch} - Training Loss : {train_loss_value} - Validation loss : {val_loss_value} - accuracy : {accuracy}%")
+            f"Epoch {epoch} - Training Loss : {train_loss_value} - Validation loss : {val_loss_value} - RMSE : {(accuracy).round(3)}")
 
     return loss_values, accuracy_values
 
 
 def test_model(model, test_data):
-    test_accuracy = 0
+    y_predicted = []
+    y_actual = []
     total = 0
     with torch.no_grad():
         for X_test, y_test in test_data:
             y_test = y_test.to(torch.float32)
             prediction = model(X_test)
             total += y_test.size(0)
-            test_accuracy += (prediction == y_test).sum().item()
+            y_predicted.extend(prediction.tolist())
+            y_actual.extend(y_test.tolist())
 
         print(
-            f"Accuracy of the model based on the test set of {len(X_test)} inputs is {(100 * test_accuracy / total)}%")
+            f"RMSE of the model based on the test set of {len(X_test)} inputs is {(mean_squared_error(y_actual, y_predicted, squared=False)).round(3)}")
 
 
 def display_metrics(loss, accuracy):
@@ -122,10 +136,10 @@ def predict(row, model):
 
 
 if __name__ == '__main__':
-    dataloader = prepare_dataloader('../data/stored_requests.csv', '../data/features_hotels.csv', batch_size=254)
-    model = Model([10,5,3, 1])
-    row = ['valletta', 20, 'german', 0, 20, 'Independant', 'Independant', 1, 5, 0]
-    #print(predict(row, model))
+    dataloader = prepare_dataloader('../dataset', '../meta_data/features_hotels.csv', batch_size=254)
+    model = Model()
+    # row = ['valletta', 20, 'german', 0, 20, 'Independant', 'Independant', 1, 5, 0]
+    # print(predict(row, model))
     metrics = train(model, dataloader[0], dataloader[2])
-    test_model(model,dataloader[1])
-    #print(metrics)
+    test_model(model, dataloader[1])
+    # print(metrics)
