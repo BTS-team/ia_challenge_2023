be52591769a345b222981b30e248f024da448ed0
Pertingens <augustinborne29@gmail.com>
2022-10-31 19:15:12 +0100

    poly

--- regression.py
+++ regression.py
@@ -1,5 +1,6 @@
 import warnings
 from sklearn import linear_model
+from sklearn.preprocessing import PolynomialFeatures
 from datascience.model import MLModel
 from datascience.utils import apply
 warnings.filterwarnings("ignore")
@@ -9,7 +10,8 @@ class Regression(MLModel):
     def __init__(self, dataset='dataset/', features_hotels='meta_data/features_hotels.csv'):
         super().__init__(dataset, features_hotels)
         self.train_set, self.valid_set = self.dataset.split(dist=[0.9])
-        self.model = linear_model.LinearRegression()
+        #self.model = linear_model.LinearRegression()
+        self.model = PolynomialFeatures(degree=1)
 
     def train(self):
         self.model.fit(self.train_set.x, self.train_set.y)
@@ -26,4 +28,4 @@ if __name__ == '__main__':
     model = Regression()
     model.train()
     print(model.validate())
-    print(model.submission())
+    #print(model.submission())
