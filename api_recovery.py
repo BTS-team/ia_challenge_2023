9adcb4e8485cd21861e6a23c1e64bd2c7e619f69
Pertingens <augustinborne29@gmail.com>
2022-11-26 10:02:33 +0100

    request

--- api_recovery.py
+++ api_recovery.py
@@ -4,12 +4,12 @@ from datascience.utils import generate_histo, get_nb_row_dataset
 params = {
         'path_requests': 'meta_data/possible_api_requests.csv',
         'path_city': 'meta_data/city_distribution.csv',
-        'nb_requests': 600,
+        'nb_requests': 391,
         'private_key': 'c760f776-e640-4d8c-a26e-dba910cc7218',
         'path_gen_request': 'meta_data/generated_requests.csv',
         'dataset': 'dataset/'
     }
-making_n_requests(**params)
+#making_n_requests(**params)
 histo, total = generate_histo('meta_data/generated_requests.csv')
 print()
 print(histo)
