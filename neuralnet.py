5aafab6d8e924fbd5614c3e7e218375d2c40fd9c
Pertingens <augustinborne29@gmail.com>
2022-12-30 15:24:37 +0100

    repet

--- neuralnet.py
+++ neuralnet.py
@@ -1,9 +1,7 @@
-from datascience.data_loading import prepare_dataloader, torch_test_set
 from datascience.model import DeepLearningModel
-import torch
-from torch.nn import Linear, MSELoss, Module, Dropout, BatchNorm1d
-from torch import flatten
-from torch.nn.functional import relu, sigmoid, tanh
+from torch.nn import Linear, Module, Dropout
+from torch.optim import RMSprop
+from torch.nn.functional import tanh
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -12,21 +10,21 @@ warnings.filterwarnings("ignore")
 class NNModel(Module):
     def __init__(self):
         super().__init__()
-        self.input = Linear(11, 22)
-        self.hid = Linear(22, 22)
-        self.hid_2 = Linear(22, 22)
-        self.output = Linear(22, 1)
+        self.input = Linear(109, 109)
+        self.hid = Linear(109, 109)
+        self.output = Linear(109, 1)
         self.dropout = Dropout(0.2)
 
     def forward(self, x):
         x = tanh(self.input(x))
+        x = self.dropout(x)
         x = tanh(self.hid(x))
-        x = tanh(self.hid_2(x))
+        x = self.dropout(x)
         return self.output(x)
 
 
 if __name__ == '__main__':
     model = NNModel()
-    nn = DeepLearningModel(model, dtype="relative")
-    nn.train(epochs=16, learning_rate=0.01, batch_size=16)
-    nn.save(path="./model", name="relative_3layer_tanh")
+    nn = DeepLearningModel(model)
+    nn.train(optimizer=RMSprop, epochs=15, learning_rate=0.001, batch_size=64, show=True)
+    nn.save(path="./model", name="relative_4layer_tanh_dropout_rms")
